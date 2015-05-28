3f51f3d770cb36341c1f08e92ae590edccebd9c9
B V Satyaram <bvsatyaram@gmail.com>
2015-05-28 12:19:27 +0530

    Calculating Result

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
 Calci = {
-  clearPreview: function() {
+  clear: function() {
     $('#preview').html("");
+    $('#result').html("0");    
   },
   deleteCharFromPreview: function() {
     var preview = $('#preview').html();
     $('#preview').html(preview.slice(0, preview.length-1));
   },
+  calculateResult: function() {
+    $('#result').html(eval($('#preview').html()));
+  },
   handleKeyPress: function(key) {
     switch($(key).text()) {
     case "AC":
-      Calci.clearPreview();
+      Calci.clear();
       break;
     case "DEL":
       Calci.deleteCharFromPreview();
       break;
     case "=":
-      console.log($(key).text());
+      Calci.calculateResult();
       break;
     default:
       $('#preview').html(
